1941d3d125daa88b4ccc7a808a857218c0645ad5
ludgoole <ludgoole@163.com>
2018-03-07 21:06:58 +0800

    add wechat_signature

---
+++ wechat/1.signature/README.md
@@ -0,0 +1,5 @@
+# wechat
+1.Node.js 接入微信公众平台开发
+2.Node.js access_token的获取、存储及更新
+3.Node.js 自定义微信菜单
+4.Node.js 微信消息管理
---
+++ wechat/1.signature/app.js
@@ -0,0 +1,53 @@
+/*
+var express = require('express');  //express 框架 
+var crypto = require('crypto');    //引入加密模块 
+var config = require('./config');  //引入配置文件
+
+
+
+//实例 express
+var app = express();
+
+//用于处理所有进入 3000 端口 get 的连接请求
+app.get('/',function(req,res){
+    //1.获取微信服务器Get请求的参数 signature、timestamp、nonce、echostr
+    var signature = req.query.signature;  //微信加密签名
+    var timestamp = req.query.timestamp;  //时间戳
+    var nonce = req.query.nonce;  //随机数
+    var echostr = req.query.echostr;//随机字符串
+
+    //2.将token、timestamp、nonce三个参数进行字典序排序
+    var array = [config.token,timestamp,nonce];
+    array.sort();
+
+    //3.将三个参数字符串拼接成一个字符串进行sha1加密
+    var tempStr = array.join('');
+    var hashCode = crypto.createHash('sha1'); //创建加密类型 
+    var resultCode = hashCode.update(tempStr,'utf8').digest('hex'); //对传入的字符串进行加密
+
+    //4.开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+    if(resultCode === signature){
+        res.send(echostr);
+    }else{
+        res.send('mismatch');
+    }
+});
+
+//监听3000端口
+app.listen(3000);
+*/
+
+const express = require('express'), //express 框架 
+       crypto = require('crypto'),  //引入加密模块
+       config = require('./config');//引入配置文件
+
+//实例 express
+var app = express();
+//用于处理所有进入 3000 端口 get 的连接请求
+app.get('/',function(req,res){
+    //对请求连接返回内容
+    res.send("Hello Node.js");
+});
+//监听3000端口
+app.listen(8080);
+
